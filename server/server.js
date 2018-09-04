885947e9f15a8b1abb808aa67e4d540b3df8198a
Can Toraman <cantoraman@gmail.com>
2018-09-04 16:45:18 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); //the adress of react app that we let in...
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat', (message) => {
+    io.sockets.emit('chat', message);
+  });
+});
+
+http.listen(3002, () => {
+  console.log('App listening on port 3002');
+});
