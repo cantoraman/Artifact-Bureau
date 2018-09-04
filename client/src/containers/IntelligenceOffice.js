e4fa4e692b7dfc586737a81d4cad70597debc5e3
Can Toraman <cantoraman@gmail.com>
2018-09-04 22:24:04 +0100

    initialize office template

---
+++ client/src/containers/IntelligenceOffice.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class IntelligenceOffice extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      artifacts = [];
+    }
+
+
+  }
+
+
+render(){
+  return(
+    <div className="intelligence-office-container">
+      <p>Hello World!</p>
+    </div>
+  );
+
+  }
+
+}
+
+export default IntelligenceOffice;
