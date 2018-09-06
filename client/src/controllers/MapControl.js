5658d8ec577c4a8e65a5ab85eef59d318fc6a3ae
Can Toraman <cantoraman@gmail.com>
2018-09-06 15:25:30 +0100

    add marker, follow up on marker scan animation

--- client/src/controllers/MapControl.js
+++ client/src/controllers/MapControl.js
@@ -1,25 +1,57 @@
-import React from 'react';
+import React,  { createRef, Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 const API_KEY = require('../helpers/api_key.js');
 
-class MapControl extends React.Component{
+class MapControl extends Component{
   constructor(props){
     super(props);
     this.state = {
-      currentLocation : null
+      currentLocation : null,
+      markered : false,
+      markerCoordinates: [],
+      scanned : false
     }
+    this.renderMarker = this.renderMarker.bind(this);
   }
 
+  mapRef = createRef()
+
+  handleClick = (event) => {
+    if(!this.state.markered){
+      this.setState({
+      markered:true,
+      markerCoordinates: [event.latlng.lat, event.latlng.lng]
+      })
+    }
+    else{
+      this.setState({
+      markerCoordinates: [event.latlng.lat, event.latlng.lng]
+      })
+    }
+    }
+
+  renderMarker(){
+    if(this.state.markered)
+    return (<Marker position={this.state.markerCoordinates}  /> )
+  }
 
   render(){
 
 
     return(
-      <Map className="map-container" center={this.props.viewedLocation} zoom={12}>
+      <Map
+        className="map-container"
+        center={this.props.viewedLocation}
+        zoom={12}
+        ref={this.mapRef}
+        onClick={this.handleClick}
+        >
+        {this.renderMarker()}
         <TileLayer
           url="https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}"
           attribution="&copy; <a href=&quot;https://www.openstreetmap.org;>OpenStreetMap</a> contributors "
           id= 'mapbox.streets'
+
           accessToken= {API_KEY}
         />
       </Map>
