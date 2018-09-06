8b5e6e38db301ed66fa77e711d07ed643d48b44a
Can Toraman <cantoraman@gmail.com>
2018-09-06 16:32:05 +0100

    set scan animation

--- client/src/controllers/MapControl.js
+++ client/src/controllers/MapControl.js
@@ -1,5 +1,6 @@
 import React,  { createRef, Component } from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Map, Marker, Popup, TileLayer, Icon } from 'react-leaflet'
+import Leaflet from 'leaflet'
 const API_KEY = require('../helpers/api_key.js');
 
 class MapControl extends Component{
@@ -31,9 +32,19 @@ class MapControl extends Component{
     }
 
   renderMarker(){
+  const image = new Leaflet.Icon({
+               iconUrl: require('../helpers/radar.gif'),
+               iconSize:     [100, 100],
+               iconAnchor:   [50, 50],
+           });
     if(this.state.markered)
-    return (<Marker position={this.state.markerCoordinates}  /> )
-  }
+    return (<Marker
+      icon= {image}
+      position={this.state.markerCoordinates}  /> )
+  };
+
+
+
 
   render(){
 
