8757eb4e9f6747354d1b3513b014b1d7c957173d
Can Toraman <cantoraman@gmail.com>
2018-10-06 15:44:37 +0100

    sets heroku

---
+++ README.md
@@ -0,0 +1,3 @@
+## Artifact Bureau
+
+Artifact Bureau is an educational web app that lets students play a treasure hunt on two browsers by doing research on matters like Geography and Culture.
--- client/src/components/WinMessage.js
+++ client/src/components/WinMessage.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const WinMessage = function (props) {
   if(props.hasWon)
-    return (<div className="win-text"><p className="stroke-behind">ALL ARTIFACTS SECURED!</p></div>)
+    return (<div className="win-text"><p className="stroke-behind">ALL ARTIFACTS ARE SECURE!</p></div>)
   return null;
 };
 
